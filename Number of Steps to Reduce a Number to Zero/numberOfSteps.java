1fcd58eeb938b3fea95cc2e198e4ad753c56f2e2
Jin Cai <jinruncai@gmail.com>
2020-04-29 17:11:10 -0700

    Number of steps to get to 0

---
+++ Number of Steps to Reduce a Number to Zero/numberOfSteps.java
@@ -0,0 +1,14 @@
+class NumberOfSteps {
+    public int numberOfSteps (int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
