7dd6ea28396919e2ef3ebf8644c5c97e84763b8d
jscholar <jinruncai@gmail.com>
2020-05-05 16:04:22 -0700

    Implement two sum
    One pass hash table solution

---
+++ Two Sum/twoSum.cs
@@ -0,0 +1,20 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var mapNumberToIndex = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            var current = nums[i];
+            var pair = target - current;
+            if (mapNumberToIndex.ContainsKey(pair)) {
+                return new int[] {mapNumberToIndex[pair], i};
+            }
+            mapNumberToIndex[current] = i;
+        }
+        return null;
+    }
+}
