9010819e7f35a5b75d3c96dbca751ca446d61e16
Jin Cai <jinruncai@gmail.com>
2020-07-02 22:08:33 -0700

    Implement Kth factor algorithm

---
+++ Kth Factor/KthFactor.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0 && --k == 0) {
+                return i;
+            }
+        }
+        if (k == 1) {
+            return n;
+        }
+        return -1;
+    }
+}
